87c01e596f08ec17bb33990c6a0bc7db041d6d90
chris brady <chrisb3546@yahoo.com>
2020-03-28 16:51:14 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,3 +11,9 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
+
+def age_appropriate_birthday(birthday_kids)
+  if birthday_kids[age] >= 12
+  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+ end
